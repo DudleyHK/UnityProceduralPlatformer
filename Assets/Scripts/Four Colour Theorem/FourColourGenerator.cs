341091562ed4efbf09ceb632f75683598c50af24
Dudley Dawes <dudley2.dawes@live.uwe.ac.uk>
2017-10-29 15:54:11 +0000

    FCT is generating in the right way.. currently not generting the colours.

--- Assets/Scripts/Four Colour Theorem/FourColourGenerator.cs
+++ Assets/Scripts/Four Colour Theorem/FourColourGenerator.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 
 
@@ -18,6 +19,8 @@ public class FourColourGenerator : MonoBehaviour
     public List<GameObject> platforms = new List<GameObject>();
     public GameObject platformPrefab;
     public short totalPlatforms = 7;
+    public float fraction = 10f;
+    public float timeToGenerate = 0f;
 
 
 
@@ -61,7 +64,7 @@ public class FourColourGenerator : MonoBehaviour
     private IEnumerator Generate()
     {
         bool createNext = true;
-
+        timeToGenerate = 0f;
         while(true)
         {
             if(platforms.Count >= totalPlatforms) break;
@@ -79,17 +82,19 @@ public class FourColourGenerator : MonoBehaviour
                     if(result)
                     {
                         createNext = true;
-                        print("create next flag is " + createNext);
+                        //print("create next flag is " + createNext);
                     }
                     else
                     {
                         createNext = false;
-                        print("create next flag is " + createNext);
+                        //print("create next flag is " + createNext);
                     }
                 }));
             }
+            timeToGenerate += Time.deltaTime;
             yield return false;
         }
+        Debug.Log("Time to generate: " + timeToGenerate);
         yield return true;
     }
 
@@ -104,35 +109,43 @@ public class FourColourGenerator : MonoBehaviour
 
     private IEnumerator FindNewPosition(Action<bool> flag)
     {
-        print("Starting find new position calculations");
+        //print("Starting find new position calculations");
 
         var lastPlatform = platforms[platforms.Count - 1];
+        var lastPlatformRenderer = lastPlatform.GetComponent<SpriteRenderer>();
+        var platformPrefabRenderer = platformPrefab.GetComponent<SpriteRenderer>();
 
-        var top    = lastPlatform.transform.position.y + lastPlatform.transform.lossyScale.y + platformPrefab.transform.lossyScale.y;    
-        var right  = lastPlatform.transform.position.x + lastPlatform.transform.lossyScale.x + platformPrefab.transform.lossyScale.x;    
-        var bottom = lastPlatform.transform.position.y - lastPlatform.transform.lossyScale.y - platformPrefab.transform.lossyScale.y;    
-        var left   = lastPlatform.transform.position.x - lastPlatform.transform.lossyScale.x - platformPrefab.transform.lossyScale.x;
+        var lastSizeOffsetX   = (lastPlatformRenderer.size.x / 2);
+        var lastSizeOffsetY   = (lastPlatformRenderer.size.y / 2);
+        var prefabSizeOffsetX = (platformPrefabRenderer.size.x / 2);
+        var prefabSizeOffsetY = (platformPrefabRenderer.size.y / 2);
 
-        print("Top position: "    + top);
-        print("Right position: "  + right);
-        print("Bottom position: " + bottom);
-        print("Left position: "   + left);
+        var top    = lastPlatform.transform.position.y + lastSizeOffsetY + prefabSizeOffsetY;    
+        var right  = lastPlatform.transform.position.x + lastSizeOffsetX + prefabSizeOffsetX;    
+        var bottom = lastPlatform.transform.position.y - lastSizeOffsetY - prefabSizeOffsetY;    
+        var left   = lastPlatform.transform.position.x - lastSizeOffsetX - prefabSizeOffsetX;
 
-        // If this produces diagonally placed platforms. Add or Subtract a 10th of the platforms size off.
-        var maxY = top    + lastPlatform.transform.lossyScale.y;
-        var minY = bottom - lastPlatform.transform.lossyScale.y;
-        var maxX = right  + lastPlatform.transform.lossyScale.x;
-        var minX = left   - lastPlatform.transform.lossyScale.x;
+        //print("Top position: "    + top);
+        //print("Right position: "  + right);
+        //print("Bottom position: " + bottom);
+        //print("Left position: "   + left);
 
-        bool intersection = false;
-        do
+        var fractionOfWidth  = lastPlatformRenderer.size.x / fraction;
+        var fractionOfHeight = lastPlatformRenderer.size.y / fraction;
+
+        var maxY = top    - fractionOfHeight;
+        var minY = bottom + fractionOfHeight;
+        var maxX = right  - fractionOfWidth;
+        var minX = left   + fractionOfWidth;
+
+        while(true)
         {
             var randomHeight = UnityEngine.Random.Range(minY, maxY);
             var randomWidth  = UnityEngine.Random.Range(minX, maxX);
 
             var newPosition = Vector2.zero;
             var randomPlacement = UnityEngine.Random.Range(1, 4);
-            print("New Random Placement is " + randomPlacement);
+            //print("New Random Placement is " + randomPlacement);
 
             switch((Place)randomPlacement)
             {
@@ -157,35 +170,34 @@ public class FourColourGenerator : MonoBehaviour
                     break;
             }
 
-            print("New position is at " + newPosition);
+            //print("New position is at " + newPosition);
             GameObject newPlatform = CreateNew(newPosition);
-            intersection = ValidatePosition(newPlatform);
+            if(PositionValid(newPlatform)) break;
 
             flag(false);
             yield return false;
        
-        } while(intersection);
-
-        print("Valid platform position found.");
+        } 
+        //print("Valid platform position found.");
         flag(true);
         yield return true;
     }
 
 
-    private bool ValidatePosition(GameObject newPlatform)
+    private bool PositionValid(GameObject newPlatform)
     {
         var bounds = newPlatform.GetComponent<SpriteRenderer>().bounds;
         foreach(var platform in platforms)
         {
             if(Overlapping(bounds, platform.GetComponent<SpriteRenderer>().bounds))
             {
-                print("Platform intersects another. Regenerate.");
+               // print("Platform intersects another. Regenerate.");
                 Destroy(newPlatform);
-                return true;
+                return false;
             }
         }
         platforms.Add(newPlatform);
-        return false;
+        return true;
     }
 
 
@@ -198,14 +210,13 @@ public class FourColourGenerator : MonoBehaviour
         return true; // bounds overlap
     }
 
-
-    private bool Overlapping(Vector2 lastPos, Vector2 newPos)
+    //// %#c  == CMD + SHIFT + C
+    [MenuItem("Tools/Clear Console %c")] // C
+    private static void ClearConsoleWindow()
     {
-        var distance = Vector2.Distance(lastPos, newPos);
-        if(distance <= 0)
-        {
-            return true;
-        }
-        return false;
+        var logEntries = Type.GetType("UnityEditorInternal.LogEntries, UnityEditor.dll");
+        var clearMethod = logEntries.GetMethod("Clear", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
+        clearMethod.Invoke(null, null);
+
     }
 }
