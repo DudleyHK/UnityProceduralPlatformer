fcc3972120dfb213e8c0cff00ad73ab90d0ff572
Dudley Dawes <dudley2.dawes@live.uwe.ac.uk>
2017-12-05 11:46:22 +0000

    created prefab sections

---
+++ Assets/Scripts/PlatformingSectionGenerator/ParseTree/PlatformGenerator.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PlatformGenerator : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public GameObject player;
+    public Vector2 instantiationMarker;
+
+    private List<GameObject> platformingSections;
+
+
+
+    private void Start()
+    {       
+        platformingSections = new List<GameObject>(Resources.LoadAll<GameObject>("PrebuiltPlatforms/"));
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            ResetLevel();
+            BuildLevel();
+        }
+    }
+
+
+    /// <summary>
+    /// Clear the list and destory world objects.
+    /// </summary>
+    private void ResetLevel()
+    {
+        if(platformingSections.Count <= 0)
+        {
+            print("No platforms to clear.");
+            return;
+        }
+
+        foreach(var section in platformingSections)
+        {
+            Destroy(section);
+        }
+        platformingSections.Clear();
+        Destroy(player);
+    }
+
+
+    private void BuildLevel()
+    {
+        PlacePlatforms();
+        PlacePlayer();
+    }
+
+
+    private void PlacePlatforms()
+    {
+        PhaseOne();
+        PhaseTwo();
+        PhaseThree();
+        PhaseFour();
+        PhaseFive();
+    }
+
+
+    private void PlacePlayer()
+    {
+
+    }
+
+
+    private void PhaseOne()
+    {
+        // run through the list and return anything with a easy value and add it to the eay list.
+        platformingSections.FindAll(obj =>
+        {
+            return (obj.GetComponent<SectionScript>().difficulty == 1);
+        });
+
+        // stretch: if objects are added to lists as the generation process progresses, each check will be quicker
+
+
+    }
+    private void PhaseTwo()
+    {
+
+    }
+    private void PhaseThree()
+    {
+
+    }
+    private void PhaseFour()
+    {
+
+    }
+    private void PhaseFive()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlatformingSectionGenerator/SectionScript.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Script which sets the dimentions of the section. Height, Length, Start position, End position.
+/// </summary>
+public class SectionScript : MonoBehaviour 
+{
+    public GameObject associatedSection;
+    public List<Transform> elements;
+    public ushort difficulty;
+    public ushort costOfFailure;
+
+    public float Height { get; set; }
+    public float Length { get; set; }
+    public float Bottom { get; set; }
+    public float Left   { get; set; }
+    public float Right  { get; set; }
+    public float Top    { get; set; }
+    public Vector3 originPosition { get; set; }
+    public Vector3 EntryPoint     { get; set; }
+    public Vector3 ExitPoint      { get; set; }
+
+
+    private void Start()
+    {
+        associatedSection = this.gameObject;
+        originPosition = associatedSection.transform.position;
+
+        /* Get the object on the far left and far right */
+        // Keep a list of all the objs which make the section.
+        associatedSection.GetComponentsInChildren(elements);
+        if(elements.Count <= 0)
+        {
+            print(associatedSection.name + " has no children with transform components");
+            return;
+        }
+
+        // remove self from list.
+        elements.Remove(transform);
+
+        // Set some basic values.
+        Left   = elements[0].position.x;
+        Right  = elements[0].position.x;
+        Top    = elements[0].position.y;
+        Bottom = elements[0].position.y;
+
+        // Go through the list and check which object has the lowest x position and highest x position.
+        foreach(var elem in elements) 
+        {
+            if(elem.position.x < Left)
+            {
+                Left = elem.position.x;
+            }
+            
+            if(elem.position.x > Right)
+            {
+                Right = elem.position.x;
+            }
+
+            if(elem.position.y < Bottom)
+            {
+                Bottom = elem.position.y;
+            }
+
+            if(elem.position.y > Top)
+            {
+                Top = elem.position.y;
+            }
+        }
+
+        // Take into account the tile size and that the centre of the tile is 
+        //   used for calculations.
+        Length = (Right - Left) + elements[0].localScale.x;
+        Height = (Top - Bottom) + elements[0].localScale.y;
+    }
+
+    private void Update()
+    {
+        originPosition = transform.position;
+    }
+}
