b58a876c1283f5a50d6ae96f1d304a31d03a6ba8
Dudley Dawes <dudley2.dawes@live.uwe.ac.uk>
2018-01-04 21:53:14 +0000

    nothing in project

---
+++ Assets/Scripts/OutputJson/GameState.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : ScriptableObject 
+{
+        public string data = "DATA YEY";
+        public ushort totalNodes = 5;
+        public string structure = "BLAH";
+
+
+}
---
+++ Assets/Scripts/OutputJson/Generate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using SimpleJSON;
+
+public class Generate : MonoBehaviour 
+{
+
+
+
+
+
+    private void Start()
+    {
+       
+    }
+}
